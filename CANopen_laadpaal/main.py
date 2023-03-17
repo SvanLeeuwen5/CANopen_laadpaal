ae58713a05847f839342d152b54fe45931f2e236
SvanLeeuwen5 <s.vanleeuwen5@student.avans.nl>
2023-03-17 16:02:51 +0100

    no comments

--- CANopen_laadpaal/main.py
+++ CANopen_laadpaal/main.py
@@ -3,11 +3,9 @@ from can.interfaces.vector import VectorBus
 import CANopen_network as cn
 
 def main():
-    # Create a network object
     network = cn.network()
-    # Connect to the network
     print(network.laadpaal.AC_Input_Current)
-    
+
 
 if __name__ == '__main__':
     main()
