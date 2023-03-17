e8c8d53797dbd048bb545d97a8195cf64260fdb7
SvanLeeuwen5 <s.vanleeuwen5@student.avans.nl>
2023-03-17 16:07:30 +0100

    todo

--- CANopen_laadpaal/CANopen_network.py
+++ CANopen_laadpaal/CANopen_network.py
@@ -3,9 +3,9 @@ from can.interfaces.vector import VectorBus
 from node import laadpaal
 
 class network(canopen.Network):
-    def __init__(self, channel=None, bitrate=None, fd=None, **kwargs):
-        super().__init__(channel=channel, bitrate=bitrate, fd=fd, **kwargs)
-        self.bus = VectorBus(channel=0, unique_hardware_id='', bitrate=0)
-        self.connect(bustype='vector', channel=0, bitrate=125000)
-        self.laadpaal = laadpaal(node_id=48, object_dictionary=None)#add object dictionary
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.bus = VectorBus(channel=0, unique_hardware_id='', bitrate=0)       #add uhid and bitrate
+        self.connect(bustype='vector', channel=0, bitrate=0)                    #add bitrate
+        self.laadpaal = laadpaal(node_id=48, object_dictionary=None)            #add object dictionary
         self.add_node(self.laadpaal)
